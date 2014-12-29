91d919d5d41eec555534c80ddaec58ac14759165
Yoann <yoann@iron-mail.net>
2014-12-29 15:34:24 +0000

    add Place & PlaceCategory fixtures

---
+++ src/PlaceFinder/Bundle/DomainBundle/DataFixtures/ORM/DataFixtureLoader.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace PlaceFinder\Bundle\DomainBundle\DataFixtures\ORM;
+
+use Hautelook\AliceBundle\Alice\DataFixtureLoader as BaseDataFixtureLoader;
+use Nelmio\Alice\Fixtures;
+
+/**
+ * Class DataFixtureLoader
+ *
+ * @package PlaceFinder\Bundle\DomainBundle\DataFixtures\ORM
+ */
+class DataFixtureLoader extends BaseDataFixtureLoader
+{
+    const PLACE_DIR = 'Place';
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function getFixtures()
+    {
+        $placeDir = sprintf('%s%s%s%s', __DIR__, DIRECTORY_SEPARATOR, self::PLACE_DIR, DIRECTORY_SEPARATOR);
+
+        return  array(
+            $placeDir . 'place_category.yml', // no dependency
+            $placeDir . 'place.yml',          // require place_category
+        );
+    }
+}
