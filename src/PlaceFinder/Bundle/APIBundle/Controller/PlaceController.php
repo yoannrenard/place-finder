4bde4b4d4d00397a2ae97f4cc58a30e9217499e7
Yoann <renard.yoann@gmail.com>
2015-05-14 16:57:34 +0100

    Install NelmioApiDoc

--- src/PlaceFinder/Bundle/APIBundle/Controller/PlaceController.php
+++ src/PlaceFinder/Bundle/APIBundle/Controller/PlaceController.php
@@ -5,9 +5,9 @@ namespace PlaceFinder\Bundle\APIBundle\Controller;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use PlaceFinder\Bundle\DomainBundle\Entity\Place;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 /**
  * Class PlaceController
@@ -17,11 +17,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class PlaceController extends FOSRestController
 {
     /**
+     * Get a list of places
+     *
      * @param ParamFetcherInterface $paramFetcher
      *
      * @return Response
      *
      * @QueryParam(name="online", requirements="(0|1)", description="The results should be online or not")
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Get a list of places",
+     *  output="Place[]"
+     * )
      */
     public function getPlacesAction(ParamFetcherInterface $paramFetcher)
     {
@@ -33,9 +41,28 @@ class PlaceController extends FOSRestController
     }
 
     /**
+     * Get a place
+     *
      * @param Place $place
      *
      * @return Response
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Get a list of places",
+     *  requirements={
+     *      {
+     *          "name"="id",
+     *          "dataType"="integer",
+     *          "requirement"="\d+",
+     *          "description"="place id"
+     *      }
+     *  },
+     *  parameters={
+     *      {"name"="id", "dataType"="integer", "required"=true, "description"="place id"}
+     *  },
+     *  output="Place"
+     * )
      */
     public function getPlaceAction(Place $place)
     {
