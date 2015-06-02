593eede5b83f2a0881c4b2937968bc224e7bcadc
Yoann <renard.yoann@gmail.com>
2015-06-02 17:01:40 +0100

    Provider service definition

--- src/PlaceFinder/Bundle/DomainBundle/Form/PlaceType.php
+++ src/PlaceFinder/Bundle/DomainBundle/Form/PlaceType.php
@@ -35,7 +35,8 @@ class PlaceType extends AbstractType
         $builder
             ->add('name')
             ->add('latitude')
-            ->add('longitude');
+            ->add('longitude')
+            ->add('isOnline');
     }
     
     /**
