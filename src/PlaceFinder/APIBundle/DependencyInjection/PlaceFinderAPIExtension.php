9121e3ec55bd72481873f6fb82fdb5c96863fd07
Yoann <renard.yoann@gmail.com>
2015-07-03 16:05:33 +0100

    Use yaml instead of xml for service definition

--- src/PlaceFinder/APIBundle/DependencyInjection/PlaceFinderAPIExtension.php
+++ src/PlaceFinder/APIBundle/DependencyInjection/PlaceFinderAPIExtension.php
@@ -22,7 +22,7 @@ class PlaceFinderAPIExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('builder.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('builder.yml');
     }
 }
